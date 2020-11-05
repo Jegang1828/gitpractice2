e46dd0796d316a6c24d8ae2787cee69b4028aaf6
Jegang1828 <jegang98@gmail.com>
2020-11-05 06:57:13 -0800

    adding multiplication to firstfile

--- firstFile.sh
+++ firstFile.sh
@@ -4,6 +4,12 @@ echo "This is first initialization"
 echo "Enter numbers you want to calculate"
 read -p "enter number 1=> " x
 read -p "enter number 2=> " y
-sum=$( x + y )
+(( sum=x + y ))
 echo "$sum"
+echo "lets subtract"
+(( sub=x - y ))
+echo "$sub"
+echo "lets multiply"
+(( mul=x * y ))
+echo "$mul"
 
