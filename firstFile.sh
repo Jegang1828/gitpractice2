2eeb519e251267ad5a043781e9195fdb197362ce
Jegang1828 <jegang98@gmail.com>
2020-11-05 06:42:29 -0800

    adding subtraction to firstfile

--- firstFile.sh
+++ firstFile.sh
@@ -2,4 +2,8 @@
 echo "This is firstFile"
 echo "This is first initialization"
 echo "Enter numbers you want to calculate"
+read -p "enter number 1=> " x
+read -p "enter number 2=> " y
+sum=$( x + y )
+echo "$sum"
 
